776454b51d4712b82ca7d809fdaf6f19ba0ff60e
c0dehard <47797039+c0dehard@users.noreply.github.com>
2019-10-06 12:09:13 +0200

    Update Datum.java

--- Datum.java
+++ Datum.java
@@ -6,18 +6,16 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 public class Datum {
-
+    
     public static void main(String[] args) {
         JFrame frame = new JFrame("Datum");
         frame.setSize(500, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
         frame.setLayout(new FlowLayout());
-
         JLabel label = new JLabel();
         frame.add(label);
         frame.setVisible(true);
-        DateTimeFormatter f = DateTimeFormatter.ofPattern  ("dd.MM.yyyy");
+        DateTimeFormatter f = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         LocalDate date = LocalDate.now();
         String txt = date.format(f);
         label.setText("<html><h1 style ='color: #000000; background-color: green'>"+txt+"</h1></html>");
